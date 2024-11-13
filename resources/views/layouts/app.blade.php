4fa6152d3fff422e28524e7905483099dc455d40
Brian Thomas <56766194+brianbct129@users.noreply.github.com>
2024-11-13 23:26:39 +0700

    fix: delete config name in layout.app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>@yield('title') - {{ config('app.name') }}</title>
+        <title>@yield('title')</title>
         <meta name="description" content="@yield('description')">
         <meta name="keywords" content="@yield('keywords', 'movies, tv shows, watch online, streaming')">
         <meta name="viewport" content="width=device-width, initial-scale=1">
